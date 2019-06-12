827d9bfe73fcb507395e3c9fd2592ba473defbb2
Nicolas Bosansky <nbosansky@sierrawireless.com>
2019-06-12 10:22:11 +0200

    PLTBUGS-7997 Overlord should not generate a new version of the configuration file if some API fails to answer (#1)
    * use go dep to handle dependencies
    * keep the current configuration file when aws api calls fail
    * add the possibility to send logs to loghost
    * Use MultiWriter to write logs to syslog and console
    * Prefix logs with "AWS API call fails" when AWS API calls fail to ease the creation of loggly alarm.
    Use AWS Session object to instanciate autoscaling and ec2 services, mandatory with the last version of the SDK.

--- overlord.go
+++ overlord.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"io"
+	"log"
+	"log/syslog"
 	"os"
 	"os/exec"
-	"log"
 	"sort"
 	"time"
+
 	// "strings"
-	"text/template"
 	"path/filepath"
+	"text/template"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
 
 	"github.com/BurntSushi/toml"
 )
 
 type Lookable interface {
-	LookupIPs() []string
+	LookupIPs() ([]string, error)
 	String() string
 }
 
@@ -31,14 +35,15 @@ func (g group) String() string {
 	return string(g)
 }
 
-func (g group) LookupIPs() []string {
-	as := autoscaling.New(nil)
-	ec := ec2.New(nil)
+func (g group) LookupIPs() ([]string, error) {
+	sess := session.Must(session.NewSession())
+	as := autoscaling.New(sess)
+	ec := ec2.New(sess)
 	autoscalingGroup := string(g)
 	envSubs := os.Getenv(string(autoscalingGroup))
 	if envSubs != "" {
 		autoscalingGroup = envSubs
-	} 
+	}
 	var output []string
 
 	params1 := &autoscaling.DescribeTagsInput{
@@ -46,7 +51,7 @@ func (g group) LookupIPs() []string {
 			{ // Required
 				Name: aws.String("Value"),
 				Values: []*string{
-					aws.String(autoscalingGroup), 
+					aws.String(autoscalingGroup),
 				},
 			},
 		},
@@ -66,10 +71,11 @@ func (g group) LookupIPs() []string {
 			// error which satisfies the awserr.Error interface.
 			log.Println(err.Error())
 		}
+		return nil, err
 	}
 
 	for _, tag := range resp1.Tags {
-		autoscalingGroup = *tag.ResourceID
+		autoscalingGroup = *tag.ResourceId
 	}
 
 	params := &autoscaling.DescribeAutoScalingGroupsInput{
@@ -94,21 +100,19 @@ func (g group) LookupIPs() []string {
 			log.Println(err.Error())
 		}
 
-		return output
+		return nil, err
 	}
 
 	for _, instance := range resp.AutoScalingGroups[0].Instances {
 
-		
-		if *instance.LifecycleState ==  "InService" {
+		if *instance.LifecycleState == "InService" {
 
 			params := &ec2.DescribeInstancesInput{
-				InstanceIDs: []*string{
-					aws.String(*instance.InstanceID), // Required
+				InstanceIds: []*string{
+					aws.String(*instance.InstanceId), // Required
 					// More values...
 				},
 			}
-
 
 			resp, err := ec.DescribeInstances(params)
 			if err != nil {
@@ -124,17 +128,17 @@ func (g group) LookupIPs() []string {
 					// error which satisfies the awserr.Error interface.
 					log.Println(err.Error())
 				}
-				continue
+				return nil, err
 			}
-		
+
 			// Pretty-print the response data.
-			output = append(output, *resp.Reservations[0].Instances[0].PrivateIPAddress)
-		
+			output = append(output, *resp.Reservations[0].Instances[0].PrivateIpAddress)
+
 		}
 
 	}
 
-	return output
+	return output, nil
 
 }
 
@@ -142,14 +146,15 @@ func (t tag) String() string {
 	return string(t)
 }
 
-func (t tag) LookupIPs() []string {
-	ec := ec2.New(nil)
+func (t tag) LookupIPs() ([]string, error) {
+	sess := session.Must(session.NewSession())
+	ec := ec2.New(sess)
 	var output []string
 	ec2tag := string(t)
 	envSubs := os.Getenv(string(ec2tag))
 	if envSubs != "" {
 		ec2tag = envSubs
-	} 
+	}
 
 	params := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
@@ -161,7 +166,6 @@ func (t tag) LookupIPs() []string {
 			},
 		},
 	}
-
 
 	resp, err := ec.DescribeInstances(params)
 	if err != nil {
@@ -177,27 +181,25 @@ func (t tag) LookupIPs() []string {
 			// error which satisfies the awserr.Error interface.
 			log.Println(err.Error())
 		}
-		return output
+		return nil, err
 	}
-		
-	for _, reservation := range resp.Reservations{
-		for _, instance := range reservation.Instances{
+
+	for _, reservation := range resp.Reservations {
+		for _, instance := range reservation.Instances {
 			if *instance.State.Name == "running" {
-				output = append(output, *instance.PrivateIPAddress)
+				output = append(output, *instance.PrivateIpAddress)
 			}
 		}
 	}
 
-	return output
+	return output, nil
 }
 
 var (
-
 	resourcesDirName = "/etc/overlord/resources"
 	templatesDirName = "/etc/overlord/templates"
 	stateFileName    = "/var/overlord/state.toml"
 	interval         = 30
-
 )
 
 type ResourceConfig struct {
@@ -220,65 +222,85 @@ func iterate() {
 	// log.Println("Start iteration")
 
 	resources := make(map[Lookable]map[*Resource]bool)
-	state     := make(map[string]map[string]bool)
+	state := make(map[string]map[string]bool)
 
 	//load resources definition files
 	resourcesDir, err := os.Open(resourcesDirName)
-	defer func(){resourcesDir.Close()}()
-	if err != nil { log.Fatal(err) }
+	defer func() { resourcesDir.Close() }()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	resourcesFiles, err := resourcesDir.Readdir(0)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for _, resourceFile := range resourcesFiles {
-		
-		if filepath.Ext(resourceFile.Name()) != ".toml" ||  resourceFile.IsDir() {continue}
-		
+
+		if filepath.Ext(resourceFile.Name()) != ".toml" || resourceFile.IsDir() {
+			continue
+		}
+
 		var rc *ResourceConfig
 		_, err := toml.DecodeFile(filepath.Join(resourcesDirName, resourceFile.Name()), &rc)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		// log.Println("Read File", resourceFile.Name(), ":", rc)
 
 		for _, group := range rc.Resource.Groups {
-			if resources[group] == nil {resources[group]=make(map[*Resource]bool)}
+			if resources[group] == nil {
+				resources[group] = make(map[*Resource]bool)
+			}
 			resources[group][&rc.Resource] = true
 		}
 
 		for _, tag := range rc.Resource.Tags {
-			if resources[tag] == nil {resources[tag]=make(map[*Resource]bool)}
+			if resources[tag] == nil {
+				resources[tag] = make(map[*Resource]bool)
+			}
 			resources[tag][&rc.Resource] = true
 		}
 	}
 
 	//load state file
 	err = os.MkdirAll(filepath.Dir(stateFileName), 0777)
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	_, err = toml.DecodeFile(stateFileName, &state)
-	if err != nil && !os.IsNotExist(err) { log.Fatal(err) }	
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatal(err)
+	}
 
 	// log.Println("Load state from", stateFileName, ":", state)
-
-
 
 	// log.Println("Find Resources to update")
 
 	resourcesToUpdate := make(map[*Resource]bool)
 	newState := make(map[string]map[string]bool)
-	
+
 	//find group ips to update
 	for g, resourcesset := range resources {
-		
+
 		//substitute group name by var env if existing
 
 		group := g.String()
-		ips := g.LookupIPs()
+		ips, err := g.LookupIPs()
+
+		// if some AWS API calls failed during the IPs lookup, stop here and exit
+		// it will keep the dest file unmodified and won't execute the reload command.
+		if err != nil {
+			log.Fatal("AWS API call fails: ", err)
+		}
 
 		newState[group] = make(map[string]bool)
 
 		changed := false
-		
+
 		for _, ip := range ips {
 			newState[group][ip] = true
 			if _, stateOk := state[group][ip]; !stateOk {
@@ -299,12 +321,10 @@ func iterate() {
 			for resource, v := range resourcesset {
 				log.Println("For group", group, "update ressource:", resource)
 				resourcesToUpdate[resource] = v
-			}			
+			}
 		}
 
-
 	}
-
 
 	//make list of ips
 	ips := make(map[string][]string)
@@ -320,55 +340,76 @@ func iterate() {
 	// log.Println("Update resources and restart processes")
 	//generate resources
 	for resource, _ := range resourcesToUpdate {
-		
+
 		tmpl, err := template.ParseFiles(filepath.Join(templatesDirName, resource.Src))
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 		err = os.MkdirAll(filepath.Dir(resource.Dest), 0777)
-	    if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
+		// create the dest file and truncate it if it already exists
 		destFile, err := os.Create(resource.Dest)
-		defer func(){destFile.Close()}()
-		if err != nil { log.Fatal(err) }
+		defer func() { destFile.Close() }()
+		if err != nil {
+			log.Fatal(err)
+		}
 		err = tmpl.Execute(destFile, ips)
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 		log.Println("For resource", resource, "update file", resource.Dest)
 
-		if resource.ReloadCmd == "" {continue}
+		if resource.ReloadCmd == "" {
+			continue
+		}
 
 		//cmdSplit := strings.Fields(resource.ReloadCmd)
 		//cmd := exec.Command(cmdSplit[0], cmdSplit[1:]...)
 		cmd := exec.Command("bash", "-c", resource.ReloadCmd)
 		log.Println(cmd)
-	    err = cmd.Start()
-	    if err != nil {
-	    	log.Fatal(err)
-	    }
-	    log.Println("For resource", resource, "start reload cmd", resource.ReloadCmd)
-	    err = cmd.Wait()
-	    if err != nil {
-	    	log.Println("For resource", resource, "reload cmd", resource.ReloadCmd, "finished with error", err)
-	    } else {
-	    	log.Println("For resource", resource, "reload cmd", resource.ReloadCmd, "finished successfuly")
-	    }
-	    
+		err = cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("For resource", resource, "start reload cmd", resource.ReloadCmd)
+		err = cmd.Wait()
+		if err != nil {
+			log.Println("For resource", resource, "reload cmd", resource.ReloadCmd, "finished with error", err)
+		} else {
+			log.Println("For resource", resource, "reload cmd", resource.ReloadCmd, "finished successfuly")
+		}
 
 	}
 
 	//write state file
 	stateFile, err := os.Create(stateFileName)
-	defer func(){stateFile.Close()}()
-	if err != nil { log.Fatal(err) }
+	defer func() { stateFile.Close() }()
+	if err != nil {
+		log.Fatal(err)
+	}
 	err = toml.NewEncoder(stateFile).Encode(&newState)
 	state = newState
 	// log.Println("Log state", state, "in file", stateFileName)
 
 	// log.Println("Iteration done")
-	
+
 }
 
-func main(){
+func main() {
+	var syslogCfg = os.Getenv("SYSLOG_ADDRESS")
+	if len(syslogCfg) > 0 {
+		syslogWriter, err := syslog.Dial("udp", syslogCfg, syslog.LOG_INFO, "av-balancing")
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(io.MultiWriter(os.Stdout, syslogWriter))
+	}
+
 	for {
 		iterate()
-		time.Sleep(time.Duration(interval)*time.Second)
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
 
