9d862289da628e25e1ae32e194af5699712f72c7
Olivier Duclos <oduclos@sierrawireless.com>
2020-02-17 15:14:18 +0100

    PLTFRS-10378: Update the build script
    - use go modules
    - go v1.13.8

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker run --rm -v "$(pwd)":/go/src/github.com/AirVantage/overlord/ -w /go/src/github.com/AirVantage/overlord/ -e GOOS=darwin -e GOARCH=amd64 golang:1.12.5 sh -c "go get && go build -v -o overlord-darwin-amd64"
-docker run --rm -v "$(pwd)":/go/src/github.com/AirVantage/overlord/ -w /go/src/github.com/AirVantage/overlord/ -e GOOS=linux -e GOARCH=amd64 golang:1.12.5 sh -c "go get && go build -v -o overlord-linux-amd64"
+docker run --rm -v "$(pwd)":/go/src/github.com/AirVantage/overlord/ -w /go/src/github.com/AirVantage/overlord/ -e GOOS=darwin -e GOARCH=amd64 golang:1.13.8 sh -c "go mod download && go build -v -o overlord-darwin-amd64"
+docker run --rm -v "$(pwd)":/go/src/github.com/AirVantage/overlord/ -w /go/src/github.com/AirVantage/overlord/ -e GOOS=linux -e GOARCH=amd64 golang:1.13.8 sh -c "go mod download && go build -v -o overlord-linux-amd64"
