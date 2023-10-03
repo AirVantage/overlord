a3bd936a03e7ed5f0c9a75938f1dcd2bb1e21a68
Benoit Plessis <bplessis@sierrawireless.com>
2023-10-03 18:11:45 +0200

    Avoid allocating a fixed slice, and filling it with 'holes', instead use append()

--- pkg/lookable/asg.go
+++ pkg/lookable/asg.go
@@ -57,12 +57,18 @@ func (asg AutoScalingGroup) LookupIPs(ipv6 bool) ([]string, error) {
 	}
 
         // Make a list of healthy instance ID in the ASG
-        instances := make([]*string, numInstances)
-        for i, inst := range resp2.AutoScalingGroups[0].Instances {
+        instances := make([]*string, 0, numInstances)
+        for _, inst := range resp2.AutoScalingGroups[0].Instances {
                 if (*inst.HealthStatus == "Healthy" && *inst.LifecycleState == "InService") {
-                        instances[i] = inst.InstanceId
+                        instances = append(instances, inst.InstanceId)
                 }
         }
+
+	// No healthy instances
+	if len(instances) == 0 {
+		return nil, nil
+	}
+
 
 	// Find running instances IP
 	params3 := &ec2.DescribeInstancesInput{
